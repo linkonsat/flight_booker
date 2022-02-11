659d068868996e26ef5c77abe1828ec674b978b3
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-10 21:55:11 -0800

    Added new flight date selection

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,11 @@ class Flight < ApplicationRecord
     has_many :bookings
     belongs_to :destination_airport, foreign_key: :destination_airport_id, class_name: "Airport"
     belongs_to :departure_airport, foreign_key: :departure_airport_id, class_name: "Airport"
+
+    #scope :flight_arrival_days, -> { Flight.all.each { |flight| flight.arrival_time.to_datetime.strftime("%d") } } 
+    def arrival_day 
+        flight = Flight.where("id = ?", id)
+        flight_day = flight.first.arrival_time.to_datetime.strftime("%d") 
+        "#{flight_day}"
+    end
 end
