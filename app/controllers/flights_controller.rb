37de3bb7c6f2f9ee960ee0f91d86817938d2003a
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-02-15 23:01:34 -0800

    Added new nav bar styling for bookings.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,10 @@
 class FlightsController < ApplicationController
     def index 
-        debugger
+        #debugger
         if(params.has_key?(:departure_id))
         @passenger_count = params[:quantity]
         @validated_flights = Flight.valid_flights(params)
-        debugger
+       # debugger
         else 
             @validated_flights = []
         end
