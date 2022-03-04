702ed34e2749b7787e3d7ccb8877e73bdfef8b1d
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-03-04 10:12:28 -0800

    Fixed dropdown bug.

--- app/javascript/controllers/flight_selector_controller.js
+++ app/javascript/controllers/flight_selector_controller.js
@@ -1,27 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="flight-selector"
+//error occurs when i "view the other list. It then appears to stop the reveal airport from running"
 export default class extends Controller {
   static targets = ["departure_airport", "arrival_airport"]
   hide_departure_airport(event) {
     let selected_id = event.srcElement.selectedIndex;
     let element = this.arrival_airportTarget.childNodes[1];
-    element[selected_id].style.display = "none"
+    this.reveal_airport(element)
     this.reveal_airport(event.srcElement)
+    element[selected_id].style.display = "none"
   }
 
   hide_arrival_airport(event) {
     let selected_id = event.srcElement.selectedIndex;
     let element = this.departure_airportTarget.childNodes[1];
-    element[selected_id].style.display = "none";
+    this.reveal_airport(element)
     this.reveal_airport(event.srcElement)
+    element[selected_id].style.display = "none"; 
   }
 
   reveal_airport(element) {
-    //this reveals the airport bug of it always displaying none
-    let loop_count = element.length - 2
-    for (let i = 0; i < loop_count; i++) {
-      console.log(element[i])
+    let loop_count = element.length 
+    for (let i = 1; i < loop_count; i++) {
       element[i].style.display = ""
     }
   }
