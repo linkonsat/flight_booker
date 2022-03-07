7f94a96c126461ecfcb2a68ebdd761d6ed3e22dd
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-03-07 11:27:20 -0800

    Fixed form overflow and text display.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,19 +5,19 @@ class Flight < ApplicationRecord
   belongs_to :destination_airport, class_name: 'Airport'
   belongs_to :departure_airport, class_name: 'Airport'
   scope :unique_flight_days, -> { Flight.select(:departure_time).distinct }
-  def arrival_day
-    all_flights = Flight.all
-    flight = Flight.where('departure_time = ?', departure_time).distinct
-    flight_day = flight.first.arrival_time.to_datetime.strftime('%y-%m-%d')
-    flight_day.to_s
+  def departure_times
+    departure_time.to_datetime.strftime('%r')
+  end
+
+  def departure_days
+    departure_time.to_datetime.strftime('%y-%m-%d')
   end
 
   def self.valid_flights(params)
-    flight_day = Flight.find(params[:flight_id]).departure_time.to_datetime.strftime('%y-%m-%d')
     found_flight_paths = Flight.where('departure_airport_id = ?', params[:departure_id]).where(
       'destination_airport_id = ?', params[:destination_id]
     )
-
+    flight_day = params[:departure_time].to_datetime.strftime('%y-%m-%d')
     flights = found_flight_paths.where('departure_time LIKE ?', "%#{flight_day}%")
   end
 end
