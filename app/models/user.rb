bae553fbdaa73236f6c4569308f1a14da04287ed
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-09 09:22:34 -0700

    Added user uniquness validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates: :email, uniqueness: true 
+  validates: :email, uniqueness: true 
+  debugger
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
