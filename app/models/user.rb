240e1f831431ed7d55a2b82664eb8b488fae81f9
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-04-10 21:50:28 -0700

    Removed typos.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates: :email, uniqueness: true 
-  validates: :email, uniqueness: true 
-  debugger
+  validates :user_name, uniqueness: true 
+  validates :email, uniqueness: true 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
