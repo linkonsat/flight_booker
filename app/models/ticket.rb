7ce58cae3c330122ef60c1edd1064512550d29a0
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-06-21 20:47:54 -0700

    Added new styling for flight search form.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,9 @@
 class Ticket < ApplicationRecord
     belongs_to :airline
     scope :random_tickets, -> { Ticket.limit(5).order("RANDOM()") }
+
+    def display_price 
+        return "$ #{self.price} from #{self.airline.name}"
+    end
 end
+
