60873c68af01de5ee19b201c0737c2b18a98c72f
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-06-21 23:49:22 -0700

    Added randomization to ticket prices.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,9 +1,5 @@
 class Ticket < ApplicationRecord
     belongs_to :airline
-    scope :random_tickets, -> { Ticket.limit(5).order("RANDOM()") }
-
-    def display_price 
-        return "$ #{self.price} from #{self.airline.name}"
-    end
+    scope :random_tickets, -> { Ticket.limit(15).order("RANDOM()").sample(5) }
 end
 
