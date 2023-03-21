0bd5aa21ce12d5d265a3ccd3a7a72edaa26e33f2
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2023-03-20 20:51:25 -0700

    Added username and email requirement.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :user_name, uniqueness: true 
-  validates :email, uniqueness: true 
+  validates :user_name, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
