d8a6c370b6db1aca3b03cbea2c51c3af555adfe6
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2023-08-06 21:58:33 -0700

    Added booking tests.

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -3,8 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-    #create the flight
-    #Create a booking
-    #then try and save the booking again
-    #It should return false as it can't have multiple sign_ups for the same flight.
+    it "Returns nil if wrong promo code present." do 
+        booking = create(:booking)
+        promotion = create(:promo_code)
+        valid_discount = Booking.discounted_flight("12345")
+        expect(valid_discount).to eql(nil)
+    end
+    it "Returns nil if no promo code exists" do 
+        booking = create(:booking)
+        valid_discount = Booking.discounted_flight("123456")
+        expect(valid_discount).to eql(nil)
+    end
+
+    it "Returns nil if the promo code has expired past 7 days." do 
+        booking = create(:booking)
+        create(:promo_code, created_at: DateTime.new(2001,2,3.5))
+        expect(Booking.discounted_flight("123456")).to eql(nil)
+    end 
+
+    it "Returns the booking if a valid promo code exists." do 
+        booking = create(:booking)
+        promo_code = create(:promo_code)
+        expect(Booking.discounted_flight("ABCDEF")).to eql(booking)
+    end
 end
