d94f55831e003a029fd8bc63b2d87091e989dc5c
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2023-08-23 09:06:05 -0700

    Added warnings and redirects to avoid user being charged.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'flights#index'
-  resources :flights
-  resources :bookings
-  resources :promo_codes
   resources :airlines
+  resources :bookings
+  resources :flights
+  resources :payments
+  resources :promo_codes
   resources :tickets
   get 'find_tickets', to: 'tickets#find_tickets'
   get 'find_flight', to: 'flights#find_flight'
